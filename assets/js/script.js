1f67ad2c85e516389fa0dadccbee905268c72df2
Adriana <84536824+itsmeadriana@users.noreply.github.com>
2021-07-31 17:43:18 -0500

    added submission and list functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,9 +5,20 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 var createTaskHandler = function(event) {
     event.preventDefault();
 
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    console.dir(taskNameInput);
+
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    console.log(taskTypeInput);
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "Next task here";
+
+    var taskInfoEl = document.createElement("div")
+    taskInfoEl.className = "task-info"
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    listItemEl.append(taskInfoEl);
     tasksToDoEl.appendChild(listItemEl);
 }
 
